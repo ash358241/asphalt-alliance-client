7a0b79e9c0e30b1b4a5c595a33f1223a9d77b469
Ashraful Islam <ashrafunit7@gmail.com>
2021-09-25 00:51:41 +0600

    regexp issue on input field solved

--- src/components/Pages/Authentication/SignIn/SignIn.jsx
+++ src/components/Pages/Authentication/SignIn/SignIn.jsx
@@ -7,7 +7,8 @@ import firebaseConfig from "../firebase.config";
 import { useHistory, useLocation } from "react-router-dom";
 import { UserContext } from "../../../../App";
 import toast, { Toaster } from "react-hot-toast";
-import Zoom from 'react-reveal/Zoom';
+import Zoom from "react-reveal/Zoom";
+import { emailRegex, passRegex } from "../../../Regexp/Regexp";
 
 const SignIn = () => {
   const [loggedInUser, setLoggedInUser] = useContext(UserContext);
@@ -30,30 +31,64 @@ const SignIn = () => {
     success: false,
   });
 
+  const [inputError, setInputError] = useState({});
+  const [regInfo, setRegInfo] = useState({});
+
   const handleBlur = (e) => {
-    let isFieldValid = true;
-    if (e.target.name === "email") {
-      isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+    // let isFieldValid = true;
+    // if (e.target.name === "email") {
+    //   isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+    // }
+    // if (e.target.name === "password") {
+    //   const isPasswordValid = e.target.value.length > 6;
+    //   const passwordHasNumber = /\d{1}/.test(e.target.value);
+    //   isFieldValid = isPasswordValid && passwordHasNumber;
+    // }
+    // if (isFieldValid) {
+    //   const newUserInfo = { ...user };
+    //   newUserInfo[e.target.name] = e.target.value;
+    //   setUser(newUserInfo);
+    // }
+    // e.preventDefault();
+
+    const inputName = e.target.name;
+    const inputValue = e.target.value;
+    const info = { ...regInfo };
+    if (inputName === "email") {
+      if (!emailRegex.test(inputValue)) {
+        setInputError({
+          name: inputName,
+          errorMessage: "Please enter your Valid e-mail !",
+        });
+        info[inputName] = null;
+        setRegInfo(info);
+      } else {
+        setInputError(null);
+        info[inputName] = inputValue;
+        setRegInfo(info);
+        setUser(info);
+      }
     }
-    if (e.target.name === "password") {
-      const isPasswordValid = e.target.value.length > 6;
-      const passwordHasNumber = /\d{1}/.test(e.target.value);
-      isFieldValid = isPasswordValid && passwordHasNumber;
+    if (inputName === "password") {
+      if (!passRegex.test(inputValue)) {
+        setInputError({
+          name: inputName,
+          errorMessage: "Please enter your Valid password !",
+        });
+        info[inputName] = null;
+        setRegInfo(info);
+      } else {
+        setInputError(null);
+        info[inputName] = inputValue;
+        setRegInfo(info);
+        setUser(info);
+      }
     }
-    if (isFieldValid) {
-      const newUserInfo = { ...user };
-      newUserInfo[e.target.name] = e.target.value;
-      setUser(newUserInfo);
-    }
-    e.preventDefault();
   };
 
-  
-
   const handleSignIn = (e) => {
-    const execute = toast.success("SignedIn Successful");
+    const execute = toast.success("Signed In Successfully");
     if (user.email && user.password) {
-      
       firebase
         .auth()
         .signInWithEmailAndPassword(user.email, user.password)
@@ -74,8 +109,8 @@ const SignIn = () => {
           const newUserInfo = { ...user };
           newUserInfo.error = error.message;
           newUserInfo.success = false;
-          toast.remove(execute)
-          toast.error(error.message)
+          toast.remove(execute);
+          toast.error(error.message);
           setUser(newUserInfo);
         });
     }
@@ -83,90 +118,100 @@ const SignIn = () => {
   };
 
   return (
-    <div style={{backgroundColor: '#F4F7FA'}}>
-    <Zoom>
-    <div
-      classNameName="signInCard"
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Toaster position="top-center" />
-      <div className="container px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
-        <div className="cardItem card0 border-0 bg-white">
-          <div className="row d-flex">
-            <div className="col-lg-6">
-              <div className="card1 pb-5">
-                <div className="row px-5 justify-content-center mt-4 mb-5 border-line">
-                  {" "}
-                  <img
-                    src="https://friendlystock.com/wp-content/uploads/2019/10/7-black-businesswoman-driving-flashy-car-cartoon-clipart.jpg"
-                    alt=""
-                    className="image"
-                  />{" "}
+    <div style={{ backgroundColor: "#F4F7FA" }}>
+      <Zoom>
+        <div
+          classNameName="signInCard"
+          style={{
+            minHeight: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Toaster position="top-center" />
+          <div className="container px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
+            <div className="cardItem card0 border-0 bg-white">
+              <div className="row d-flex">
+                <div className="col-lg-6">
+                  <div className="card1 pb-5">
+                    <div className="row px-5 justify-content-center mt-4 mb-5 border-line">
+                      {" "}
+                      <img
+                        src="https://friendlystock.com/wp-content/uploads/2019/10/7-black-businesswoman-driving-flashy-car-cartoon-clipart.jpg"
+                        alt=""
+                        className="image"
+                      />{" "}
+                    </div>
+                  </div>
                 </div>
+                <form onSubmit={handleSignIn} className="col-lg-6">
+                  <div className="card2 cardItem border-0 px-4 py-5">
+                    <div className="row px-3">
+                      {" "}
+                      <label className="mb-1">
+                        <h6 className="mb-0 text-sm">Email Address</h6>
+                      </label>{" "}
+                      <input
+                        className="mb-4"
+                        type="email"
+                        name="email"
+                        placeholder="Enter a valid email address"
+                        onChange={handleBlur}
+                      />{" "}
+                      {inputError?.name === "email" && (
+                        <p className="text-danger text-center">
+                          {inputError?.errorMessage}
+                        </p>
+                      )}
+                    </div>
+                    <div className="row px-3">
+                      {" "}
+                      <label className="mb-1">
+                        <h6 className="mb-0 text-sm">Password</h6>
+                      </label>{" "}
+                      <input
+                        type="password"
+                        name="password"
+                        placeholder="Enter password"
+                        onChange={handleBlur}
+                      />{" "}
+                      {inputError?.name === "password" && (
+                        <p className="text-danger text-center">
+                          {inputError?.errorMessage}
+                        </p>
+                      )}
+                    </div>
+                    <div className="row my-4 px-3">
+                      {" "}
+                      <button
+                        type="submit"
+                        className="logBtn btn btn-primary text-center"
+                      >
+                        Login
+                      </button>{" "}
+                    </div>
+                    <div className="row mb-4 px-3">
+                      {" "}
+                      <small className="font-weight-bold">
+                        Don't have an account?{" "}
+                        <Link to="/signUp" className="text-danger ">
+                          Register
+                        </Link>
+                      </small>{" "}
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
-            <form onSubmit={handleSignIn} className="col-lg-6">
-              <div className="card2 cardItem border-0 px-4 py-5">
-                <div className="row px-3">
-                  {" "}
-                  <label className="mb-1">
-                    <h6 className="mb-0 text-sm">Email Address</h6>
-                  </label>{" "}
-                  <input
-                    className="mb-4"
-                    type="email"
-                    name="email"
-                    placeholder="Enter a valid email address"
-                    onBlur={handleBlur}
-                  />{" "}
-                </div>
-                <div className="row px-3">
-                  {" "}
-                  <label className="mb-1">
-                    <h6 className="mb-0 text-sm">Password</h6>
-                  </label>{" "}
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="Enter password"
-                    onBlur={handleBlur}
-                  />{" "}
-                </div>
-                <div className="row my-4 px-3">
-                  {" "}
-                  <button
-                    type="submit"
-                    className="logBtn btn btn-primary text-center"
-                  >
-                    Login
-                  </button>{" "}
-                </div>
-                <div className="row mb-4 px-3">
-                  {" "}
-                  <small className="font-weight-bold">
-                    Don't have an account?{" "}
-                    <Link to="/signUp" className="text-danger ">
-                      Register
-                    </Link>
-                  </small>{" "}
-                </div>
-              </div>
-            </form>
+            <button className="btn btn-info text-light mt-4">
+              <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                Back To Home
+              </Link>
+            </button>
           </div>
         </div>
-        <button className="btn btn-info text-light mt-4">
-          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            Back To Home
-          </Link>
-        </button>
-      </div>
-    </div>
-    </Zoom>
+      </Zoom>
     </div>
   );
 };
--- src/components/Pages/Authentication/SignUp/SignUp.jsx
+++ src/components/Pages/Authentication/SignUp/SignUp.jsx
@@ -1,172 +1,234 @@
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import '../SignIn/SignIn.css';
-// import {emailRegex, passRegex} from '../../../RegEx/RegEx';
+import "../SignIn/SignIn.css";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
-import firebaseConfig from '../firebase.config'; 
-import { useHistory, useLocation } from 'react-router-dom';
+import firebaseConfig from "../firebase.config";
+import { useHistory, useLocation } from "react-router-dom";
 import { UserContext } from "../../../../App";
 import toast, { Toaster } from "react-hot-toast";
-import Zoom from 'react-reveal/Zoom';
-
+import Zoom from "react-reveal/Zoom";
+import { emailRegex, passRegex } from "../../../Regexp/Regexp";
 
 const SignUp = () => {
   const [loggedInUser, setLoggedInUser] = useContext(UserContext);
 
   const history = useHistory();
-    const location = useLocation();
+  const location = useLocation();
 
-    const { from } = location.state || { from: { pathname: "signIn" } };
+  const { from } = location.state || { from: { pathname: "signIn" } };
 
-    if (!firebase.apps.length) {
-      firebase.initializeApp(firebaseConfig);
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
   } else {
-      firebase.app();
+    firebase.app();
   }
 
   const [user, setUser] = useState({
     isSignedIn: false,
-    name: '',
-    email: '',
-    password: '',
-    error: '',
+    name: "",
+    email: "",
+    password: "",
+    error: "",
     success: false,
-});
+  });
 
-// const [newUser, setNewUser] = useState(false);
+  const [inputError, setInputError] = useState({});
+  const [regInfo, setRegInfo] = useState({});
 
-//field validating
-const handleBlur = (e) => {
-    let isFieldValid = true;
-    if(e.target.name === "email"){
-        isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
-    }
-    if(e.target.name === "password"){
-        const isPasswordValid = e.target.value.length > 6;
-        const passwordHasNumber =  /\d{1}/.test(e.target.value);
-        isFieldValid = isPasswordValid && passwordHasNumber; 
-    }
-    if(isFieldValid){
-        const newUserInfo = {...user};
-        newUserInfo[e.target.name] = e.target.value;
-        setUser(newUserInfo);
-    }
-    e.preventDefault();
-}
+  const handleBlur = (e) => {
+    // let isFieldValid = true;
+    // if(e.target.name === "email"){
+    //     isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+    // }
+    // if(e.target.name === "password"){
+    //     const isPasswordValid = e.target.value.length > 6;
+    //     const passwordHasNumber =  /\d{1}/.test(e.target.value);
+    //     isFieldValid = isPasswordValid && passwordHasNumber;
+    // }
+    // if(isFieldValid){
+    //     const newUserInfo = {...user};
+    //     newUserInfo[e.target.name] = e.target.value;
+    //     setUser(newUserInfo);
+    // }
+    // e.preventDefault();
 
-//SignIn or SignUp
-const handleRegister = (e) => {
-  const success = toast.success("SignedUp Successful");
-    if(user.email && user.password){
-        firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+    const inputName = e.target.name;
+    const inputValue = e.target.value;
+    const info = { ...regInfo };
+    if (inputName === "email") {
+      if (!emailRegex.test(inputValue)) {
+        setInputError({
+          name: inputName,
+          errorMessage: "Please enter a Valid e-mail !",
+        });
+        info[inputName] = null;
+        setRegInfo(info);
+      } else {
+        setInputError(null);
+        info[inputName] = inputValue;
+        setRegInfo(info);
+        setUser(info);
+      }
+    }
+    if (inputName === "password") {
+      if (!passRegex.test(inputValue)) {
+        setInputError({
+          name: inputName,
+          errorMessage: "Please enter a Valid password with min 8 letters having at least a symbol, upper and lower case letter and a number !",
+        });
+        info[inputName] = null;
+        setRegInfo(info);
+      } else {
+        setInputError(null);
+        info[inputName] = inputValue;
+        setRegInfo(info);
+        setUser(info);
+      }
+    }
+
+  };
+
+  //SignIn or SignUp
+  const handleRegister = (e) => {
+    const success = toast.success("Signed Up Successfully");
+    if (user.email && user.password) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(user.email, user.password)
         .then((res) => {
-        const newUserInfo = {...user};
-        newUserInfo.error = '';
-        newUserInfo.success = true;
-        setUser(newUserInfo);
-        updateUserName(user.name);
-        toast.success(success);
-        setLoggedInUser(newUserInfo);
-        history.replace(from);
+          const newUserInfo = { ...user };
+          newUserInfo.error = "";
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+          updateUserName(user.name);
+          toast.success(success);
+          setLoggedInUser(newUserInfo);
+          history.replace(from);
         })
         .catch((error) => {
-            const newUserInfo = {...user};
-            newUserInfo.error = error.message;
-            newUserInfo.success = false;
-            toast.remove(success)
-            toast.error(error.message)
-            setUser(newUserInfo);
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          newUserInfo.success = false;
+          toast.remove(success);
+          toast.error(error.message);
+          setUser(newUserInfo);
         });
     }
     e.preventDefault();
-}
+  };
 
-//update user
-const updateUserName = (name) => {
+  //update user
+  const updateUserName = (name) => {
     const user = firebase.auth().currentUser;
 
-    user.updateProfile({
-    displayName: name
-    }).then(function() {
-    console.log('Update successful')
-    }).catch(function(error) {
+    user
+      .updateProfile({
+        displayName: name,
+      })
+      .then(function () {
+        console.log("Update successful");
+      })
+      .catch(function (error) {
         console.log(error);
-    });
-}
+      });
+  };
 
   return (
-    <div style={{backgroundColor: '#F4F7FA'}}>
-    <Zoom>
-    <div classNameName="signUpCard" style={{minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-       <Toaster position="top-center" reverseOrder={false} />
-      <div className="container px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
-        <div className="cardItem card0 border-0 bg-white">
-          <div className="row d-flex">
-            <div className="col-lg-6">
-              <div className="card1 pb-5">
-                <div className="row px-5 justify-content-center mt-4 mb-5 border-line">
-                  {" "}
-                  <img
-                    src="https://friendlystock.com/wp-content/uploads/2019/10/7-black-businesswoman-driving-flashy-car-cartoon-clipart.jpg"
-                    alt=""
-                    className="image"
-                  />{" "}
+    <div style={{ backgroundColor: "#F4F7FA" }}>
+      <Zoom>
+        <div
+          classNameName="signUpCard"
+          style={{
+            minHeight: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Toaster position="top-center" reverseOrder={false} />
+          <div className="container px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
+            <div className="cardItem card0 border-0 bg-white">
+              <div className="row d-flex">
+                <div className="col-lg-6">
+                  <div className="card1 pb-5">
+                    <div className="row px-5 justify-content-center mt-4 mb-5 border-line">
+                      {" "}
+                      <img
+                        src="https://friendlystock.com/wp-content/uploads/2019/10/7-black-businesswoman-driving-flashy-car-cartoon-clipart.jpg"
+                        alt=""
+                        className="image"
+                      />{" "}
+                    </div>
+                  </div>
                 </div>
+                <form onSubmit={handleRegister} className="col-lg-6">
+                  <div className="card2 cardItem border-0 px-4 py-5">
+                    <div className="row px-3">
+                      {" "}
+                      <label className="mb-1">
+                        <h6 className="mb-0 text-sm">Email Address</h6>
+                      </label>{" "}
+                      <input
+                        className="mb-4"
+                        type="email"
+                        name="email"
+                        placeholder="Enter a valid email address"
+                        onChange={handleBlur}
+                      />{" "}
+                      {inputError?.name === "email" && (
+                        <p className="text-danger text-center">
+                          {inputError?.errorMessage}
+                        </p>
+                      )}
+                    </div>
+                    <div className="row px-3">
+                      {" "}
+                      <label className="mb-1">
+                        <h6 className="mb-0 text-sm">Password</h6>
+                      </label>{" "}
+                      <input
+                        type="password"
+                        name="password"
+                        placeholder="Enter password"
+                        onChange={handleBlur}
+                      />{" "}
+                      {inputError?.name === "password" && (
+                        <p className="text-danger text-center">
+                          {inputError?.errorMessage}
+                        </p>
+                      )}
+                    </div>
+                    <div className="row my-4 px-3">
+                      {" "}
+                      <button
+                        type="submit"
+                        className="logBtn btn btn-primary text-center"
+                      >
+                        Register
+                      </button>{" "}
+                    </div>
+                    <div className="row mb-4 px-3">
+                      {" "}
+                      <small className="font-weight-bold">
+                        Already have an account?{" "}
+                        <Link to="/signIn" className="text-danger ">
+                          SignIn
+                        </Link>
+                      </small>{" "}
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
-            <form onSubmit={handleRegister} className="col-lg-6">
-              <div className="card2 cardItem border-0 px-4 py-5" >
-                <div className="row px-3">
-                  {" "}
-                  <label className="mb-1">
-                    <h6 className="mb-0 text-sm">Email Address</h6>
-                  </label>{" "}
-                  <input
-                    className="mb-4"
-                    type="email"
-                    name="email"
-                    placeholder="Enter a valid email address"
-                    onBlur={handleBlur}
-                  />{" "}
-                </div>
-                <div className="row px-3">
-                  {" "}
-                  <label className="mb-1">
-                    <h6 className="mb-0 text-sm">Password</h6>
-                  </label>{" "}
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="Enter password"
-                    onBlur={handleBlur}
-                  />{" "}
-                </div>
-                <div className="row my-4 px-3">
-                  {" "}
-                  <button type="submit" className="logBtn btn btn-primary text-center">
-                    Register
-                  </button>{" "}
-                </div>
-                <div className="row mb-4 px-3">
-                  {" "}
-                  <small className="font-weight-bold">
-                    Already have an account? <Link to="/signIn" className="text-danger ">SignIn</Link>
-                  </small>{" "}
-                </div>
-              </div>
-              
-            </form>
+            <button className="btn btn-info text-light mt-4">
+              <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                Back To Home
+              </Link>
+            </button>
           </div>
         </div>
-        <button className="btn btn-info text-light mt-4">
-          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            Back To Home
-          </Link>
-        </button>
-      </div>
-    </div>
-    </Zoom>
+      </Zoom>
     </div>
   );
 };
