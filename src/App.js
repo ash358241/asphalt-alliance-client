efd137fbddd1cc9d6686779a8158299ff16f6848
Ashraful Islam <ashrafunit7@gmail.com>
2021-09-28 01:23:33 +0600

    private route generated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import './App.css';
-import Home from './components/Pages/Home/Home';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Entries from './components/Pages/DashBoard/Entries/Entries';
-import EntryDetails from './components/EntryDetails/EntryDetails';
-import SignIn from './components/Pages/Authentication/SignIn/SignIn';
-import SignUp from './components/Pages/Authentication/SignUp/SignUp';
-import { createContext, useState } from 'react';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-import ChatBox from './components/ChatBox/ChatBox';
-import SpecificEntry from './components/SpecificEntry/SpecificEntry';
+import "./App.css";
+import Home from "./components/Pages/Home/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Entries from "./components/Pages/DashBoard/Entries/Entries";
+import EntryDetails from "./components/EntryDetails/EntryDetails";
+import SignIn from "./components/Pages/Authentication/SignIn/SignIn";
+import SignUp from "./components/Pages/Authentication/SignUp/SignUp";
+import { createContext, useState } from "react";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import ChatBox from "./components/ChatBox/ChatBox";
+import SpecificEntry from "./components/SpecificEntry/SpecificEntry";
 
 export const UserContext = createContext();
 function App() {
@@ -22,35 +17,34 @@ function App() {
   return (
     <div className="app">
       <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
-      <Router>
-        <Switch>
-          <Route path="/home">
-          <Home></Home>
-          </Route>
-          <PrivateRoute path="/dashboard">
-          <Entries></Entries>
-          {/* <SpecificEntry></SpecificEntry> */}
-          </PrivateRoute>
-          <Route path="/entry/:entryId">
-          <EntryDetails></EntryDetails>
-        </Route>
-        <Route path="/specificEntry">
-          <SpecificEntry></SpecificEntry>
-        </Route>
-        <Route path="/signIn">
-          <SignIn></SignIn>
-        </Route>
-        <Route path="/signUp">
-          <SignUp></SignUp>
-        </Route>
-        <Route path="/chat">
-          <ChatBox></ChatBox>
-        </Route>
-          <Route exact path="/">
-          <Home></Home>
-          </Route>
-        </Switch>
-      </Router>
+        <Router>
+          <Switch>
+            <Route path="/home">
+              <Home></Home>
+            </Route>
+            <PrivateRoute path="/dashboard">
+              <Entries></Entries>
+            </PrivateRoute>
+            <PrivateRoute path="/entry/:entryId">
+              <EntryDetails></EntryDetails>
+            </PrivateRoute>
+            <PrivateRoute path="/specificEntry">
+              <SpecificEntry></SpecificEntry>
+            </PrivateRoute>
+            <Route path="/signIn">
+              <SignIn></SignIn>
+            </Route>
+            <Route path="/signUp">
+              <SignUp></SignUp>
+            </Route>
+            <Route path="/chat">
+              <ChatBox></ChatBox>
+            </Route>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+          </Switch>
+        </Router>
       </UserContext.Provider>
     </div>
   );
